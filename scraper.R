92c543ce450dae7e906b01214d0f3e96d53efda7
John Laing <john.laing@gmail.com>
2017-05-31 22:38:35 -0400

    do downloads

--- scraper.R
+++ scraper.R
@@ -1,6 +1,10 @@
+## directory to download reports
+dl.dir <- file.path(getwd(), paste0("downloads.", format(Sys.time(), "%Y%m%d.%H%M%S")))
+dir.create(dl.dir)
+
 ## connect to server and navigate to page
 require(RSelenium)
-rd <- rsDriver(browser="chrome")
+rd <- rsDriver(browser="chrome", extraCapabilities=list(chromeOptions=list(prefs=list("download.default_directory"=dl.dir))))
 cl <- rd$client
 cl$navigate("https://dashboards.lordashcroftpolls.com/login.aspx?target=1rH46DzH68RfFl7AknVWbbl4nsH0s%2f%2fj5uXrUWFycQ4%3d")
 
@@ -32,3 +36,51 @@ stopifnot(length(dd.elems) == 633)
 ## drop first "no selection"
 stopifnot(dd.elem.name(dd.elems[[1]]) == "No selection")
 dd.elems <- dd.elems[-1]
+
+
+
+## export
+export.btn <- cl$findElement(using="css", "div#btnOpenExportPanel")
+export.btn$clickElement(); export.btn$clickElement() ## open and close to load panel
+excel.btn <- cl$findElement(using="css", "input#downloadSelectedPPTExcel")
+
+dl <- function(max.wait=30) {
+    ## generate and wait for link
+    export.btn$clickElement()
+    len <- length(cl$findElements(using="css", "div#downloadReportsPowerpoint > a"))
+    excel.btn$clickElement()
+    for (i in seq(max.wait)) {
+        lnk <- cl$findElements(using="css", "div#downloadReportsPowerpoint > a")
+        if (length(lnk) > len) break
+        Sys.sleep(1)
+    }
+    if (length(lnk) <= len) {
+        excel.btn$clickElement()
+        stop("timeout: waiting for link")
+    }
+    lnk <- lnk[[length(lnk)]]
+
+    ## download and wait
+    lnk$clickElement()
+    export.btn$clickElement()
+    for (i in seq(max.wait)) {
+        f <- list.files(dl.dir, "^Welcome.*\\.zip$", full.names=TRUE)
+        if (length(f) == 1) return(f)
+        if (length(f) > 1) stop("multiple files match")
+        Sys.sleep(1)
+    }
+    stop("timeout: waiting for download")
+}
+
+
+## generate and download all
+for (i in seq_along(dd.elems)) {
+    nm <- dd.elem.name(dd.elems[[i]])
+    cat(nm, "\n")
+    select.const(dd.elems[[i]])
+    f <- dl()
+
+    ## rename
+    new.nm <- file.path(dl.dir, paste0(nm, ".zip"))
+    file.rename(f, new.nm)
+}
